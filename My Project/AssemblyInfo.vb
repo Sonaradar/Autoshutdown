c4938580900da64c0548b3625d2aa7a7a4630d5e
Sonaradar <44935310+Sonaradar@users.noreply.github.com>
2020-04-04 17:06:03 +0800

    Add files via upload

---
+++ My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' 有关程序集的一般信息由以下
+' 控制。更改这些特性值可修改
+' 与程序集关联的信息。
+
+'查看程序集特性的值
+
+<Assembly: AssemblyTitle("AutoShutdown")>
+<Assembly: AssemblyDescription("New 5th-Series")>
+<Assembly: AssemblyCompany("Thunder Software Corporation")>
+<Assembly: AssemblyProduct("AutoShutdown 500 Series")>
+<Assembly: AssemblyCopyright("Copyright © 2018-2020 Thunder Software")>
+<Assembly: AssemblyTrademark("Thunder Software Corporation")>
+
+<Assembly: ComVisible(True)>
+
+'如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
+<Assembly: Guid("3ba9655a-0b75-48f2-8b97-361960751758")>
+
+' 程序集的版本信息由下列四个值组成: 
+'
+'      主版本
+'      次版本
+'      生成号
+'      修订号
+'
+' 可以指定所有值，也可以使用以下所示的 "*" 预置版本号和修订号
+' 方法是按如下所示使用“*”: :
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("5.1.1.0")>
+<Assembly: AssemblyFileVersion("5.1.1.0")>
